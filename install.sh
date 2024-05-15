42965a92b11cb86c5b65f4d98d6ed767ddae5515
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2024-05-15 17:56:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,8 +41,9 @@ go install -v github.com/projectdiscovery/katana/cmd/katana@latest
 go install -v github.com/jaeles-project/gospider@latest
 go install -v github.com/hakluke/hakrawler@latest
 go install -v github.com/ropnop/kerbrute@latest
-go install github.com/lc/gau/v2/cmd/gau@latest
-go install github.com/utkusen/socialhunter@latest
+go install -v github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/utkusen/socialhunter@latest
+go install -v github.com/lc/subjs@latest
 sleep 2
 
 # install more tools
